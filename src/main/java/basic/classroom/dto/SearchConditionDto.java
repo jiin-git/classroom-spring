affa5ff3bed52cb5a72c466736f46ccba1316046
Jiin <jiin.bae99@gmail.com>
2023-07-26 23:12:08 +0900

    :art: pagination 기능 추가에 따른 SearchConditionDto 구조 변경

--- src/main/java/basic/classroom/dto/SearchConditionDto.java
+++ src/main/java/basic/classroom/dto/SearchConditionDto.java
@@ -13,10 +13,12 @@ public class SearchConditionDto {
     private String status;
     private String condition;
     private String text;
+    private Long page;
 
-    public SearchConditionDto(String status, String condition, String text) {
+    public SearchConditionDto(String status, String condition, String text, Long page) {
         this.status = status;
         this.condition = condition;
         this.text = text;
+        this.page = page;
     }
 }
