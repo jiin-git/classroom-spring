5eace7a7845050e885a11541def91ca84d01b7b2
Jiin <jiin.bae99@gmail.com>
2023-07-31 22:06:55 +0900

    :safety_vest: StoreImageException 추가

---
+++ src/main/java/basic/classroom/exception/StoreImageException.java
@@ -0,0 +1,27 @@
+package basic.classroom.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class StoreImageException extends RuntimeException {
+    public StoreImageException() {
+        super();
+    }
+
+    public StoreImageException(String message) {
+        super(message);
+    }
+
+    public StoreImageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StoreImageException(Throwable cause) {
+        super(cause);
+    }
+
+    protected StoreImageException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
