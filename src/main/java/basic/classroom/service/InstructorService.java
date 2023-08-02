bf15364852c54a8594cb1e8df9336783c11a3bfc
Jiin <jiin.bae99@gmail.com>
2023-08-02 16:03:52 +0900

    :safety_vest: Add validation related to profile img and extract method

--- src/main/java/basic/classroom/service/InstructorService.java
+++ src/main/java/basic/classroom/service/InstructorService.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.text.Normalizer;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -53,25 +54,10 @@ public class InstructorService {
 
         if (updateMemberDto.getImageFile() != null && !updateMemberDto.getImageFile().isEmpty()) {
             try {
-                MultipartFile imageFile = updateMemberDto.getImageFile();
-                String imageName = Normalizer.normalize(imageFile.getOriginalFilename(), Normalizer.Form.NFC);
-                String contentType = imageFile.getContentType();
-                byte[] imageBytes = imageFile.getBytes();
-
-                instructor.setProfileImage(new ProfileImage(imageName, contentType, imageBytes));
+                saveInstructorImageFile(updateMemberDto.getImageFile(), instructor);
             } catch (IOException e) {
                 throw new StoreImageException("프로필 이미지를 저장할 수 없습니다. 이미지 형식과 사이즈를 다시 확인해주세요.", e);
             }
-        }
-    }
-
-    @Transactional
-    public void initializeProfile(Long id) {
-        Instructor instructor = instructorRepository.findOne(id);
-        if (instructor.getProfileImage() != null) {
-            instructor.getProfileImage().setImageName(null);
-            instructor.getProfileImage().setDataType(null);
-            instructor.getProfileImage().setImageData(null);
         }
     }
 
@@ -85,6 +71,14 @@ public class InstructorService {
     public Long addLecture(Long instructorId, AddLectureDto addLectureDto) {
         Instructor instructor = instructorRepository.findOne(instructorId);
         Lecture lecture = Lecture.createLecture(addLectureDto.getName(), instructor, addLectureDto.getPersonnel(), addLectureDto.getLectureStatus());
+
+        if (addLectureDto.getImageFile() != null && !addLectureDto.getImageFile().isEmpty()) {
+            try {
+                saveLectureImageFile(addLectureDto.getImageFile(), lecture);
+            } catch (IOException e) {
+                throw new StoreImageException("프로필 이미지를 저장할 수 없습니다. 이미지 형식과 사이즈를 다시 확인해주세요.", e);
+            }
+        }
 
         lectureRepository.save(lecture);
         instructor.addLectures(lecture);
@@ -101,14 +95,9 @@ public class InstructorService {
         lecture.setRemainingPersonnel(updateRemainingPersonnel);
         lecture.setLectureStatus(updateLectureDto.getLectureStatus());
 
-        if (!updateLectureDto.getImageFile().isEmpty()) {
+        if (updateLectureDto.getImageFile() != null && !updateLectureDto.getImageFile().isEmpty()) {
             try {
-                MultipartFile imageFile = updateLectureDto.getImageFile();
-                String imageName = Normalizer.normalize(imageFile.getOriginalFilename(), Normalizer.Form.NFC);
-                String contentType = imageFile.getContentType();
-                byte[] imageBytes = imageFile.getBytes();
-
-                lecture.setProfileImage(new ProfileImage(imageName, contentType, imageBytes));
+                saveLectureImageFile(updateLectureDto.getImageFile(), lecture);
             } catch (IOException e) {
                 throw new StoreImageException("프로필 이미지를 저장할 수 없습니다. 이미지 형식과 사이즈를 다시 확인해주세요.", e);
             }
@@ -118,5 +107,53 @@ public class InstructorService {
     public List<Lecture> findLectures(Long id) {
         List<Lecture> lectures = instructorRepository.findOne(id).getLectures().values().stream().toList();
         return lectures;
+    }
+
+    @Transactional
+    public void initializeProfile(Long id) {
+        Instructor instructor = instructorRepository.findOne(id);
+        if (instructor.getProfileImage() != null) {
+            instructor.getProfileImage().setImageName(null);
+            instructor.getProfileImage().setDataType(null);
+            instructor.getProfileImage().setImageData(null);
+        }
+    }
+
+    private static void saveInstructorImageFile(MultipartFile imageFile, Instructor instructor) throws IOException {
+        ProfileImage profileImage = validProfileImage(imageFile);
+        instructor.setProfileImage(profileImage);
+    }
+
+    private static void saveLectureImageFile(MultipartFile imageFile, Lecture lecture) throws IOException {
+        ProfileImage profileImage = validProfileImage(imageFile);
+        lecture.setProfileImage(profileImage);
+    }
+
+    private static ProfileImage validProfileImage(MultipartFile imageFile) throws IOException {
+        ProfileImage profileImage = getProfileImage(imageFile);
+        List<String> validImageTypes = getValidImageTypes();
+        if (!validImageTypes.contains(imageFile.getContentType())) {
+            throw new StoreImageException();
+        }
+
+        return profileImage;
+    }
+
+    private static ProfileImage getProfileImage(MultipartFile imageFile) throws IOException {
+        String imageName = Normalizer.normalize(imageFile.getOriginalFilename(), Normalizer.Form.NFC);
+        String contentType = imageFile.getContentType();
+        byte[] imageBytes = imageFile.getBytes();
+
+        return new ProfileImage(imageName, contentType, imageBytes);
+    }
+
+    private static List<String> getValidImageTypes() {
+        List<String> validImageTypes = new ArrayList<>();
+        ValidImageType[] values = ValidImageType.values();
+        for (ValidImageType value : values) {
+            String dataType = "image/" + value.toString().toLowerCase();
+            validImageTypes.add(dataType);
+        }
+        return validImageTypes;
     }
 }
