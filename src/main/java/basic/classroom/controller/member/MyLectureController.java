8e93694fb8f386da8139431fbd2df03fc8fc70ca
Jiin <jiin.bae99@gmail.com>
2023-08-07 16:10:37 +0900

    :art: Repository 계층에서 paging 기능 추가에 따른 Controller 계층 변경사항 반영

--- src/main/java/basic/classroom/controller/member/MyLectureController.java
+++ src/main/java/basic/classroom/controller/member/MyLectureController.java
@@ -5,6 +5,7 @@ import basic.classroom.domain.Instructor;
 import basic.classroom.domain.Lecture;
 import basic.classroom.domain.LectureStudentMapper;
 import basic.classroom.domain.Student;
+import basic.classroom.dto.PageDto;
 import basic.classroom.service.LectureService;
 import basic.classroom.service.MemberService;
 import basic.classroom.service.PagingService;
@@ -28,24 +29,37 @@ public class MyLectureController {
     private final PagingService pagingService;
     private final MemberService memberService;
 
+//    @GetMapping("/instructor/lectures")
+//    public String instructorMyLecture(@RequestParam(required = false) Long page, HttpSession session, Model model) {
+//        Instructor instructor = findInstructor(session);
+//        List<Lecture> lectures = lectureService.findAllLectures(instructor);
+//
+//        pagingLecturesModel(page, model, lectures);
+//        model.addAttribute("instructor", instructor);
+//
+//        return "member/instructor/lectureList";
+//    }
     @GetMapping("/instructor/lectures")
     public String instructorMyLecture(@RequestParam(required = false) Long page, HttpSession session, Model model) {
         Instructor instructor = findInstructor(session);
-        List<Lecture> lectures = lectureService.findAllLectures(instructor);
-
-        pagingLecturesModel(page, model, lectures);
-        model.addAttribute("instructor", instructor);
-
+        pagingMyLectures(page, model, instructor);
         return "member/instructor/lectureList";
     }
+
+//    @GetMapping("/student/lectures")
+//    public String studentMyLecture(@RequestParam(required = false) Long page, HttpSession session, Model model) {
+//        Student student = findStudent(session);
+//        List<Lecture> lectures = lectureService.findAllLectures(student);
+//
+//        pagingLecturesModel(page, model, lectures);
+//        model.addAttribute("student", student);
+//
+//        return "member/student/lectureList";
+//    }
     @GetMapping("/student/lectures")
     public String studentMyLecture(@RequestParam(required = false) Long page, HttpSession session, Model model) {
         Student student = findStudent(session);
-        List<Lecture> lectures = lectureService.findAllLectures(student);
-
-        pagingLecturesModel(page, model, lectures);
-        model.addAttribute("student", student);
-
+        pagingMyLectures(page, model, student);
         return "member/student/lectureList";
     }
 
@@ -72,20 +86,68 @@ public class MyLectureController {
         return "member/instructor/applicantsInfo";
     }
 
-    private void pagingLecturesModel(Long page, Model model, List<Lecture> lectures) {
-        int lecturesCnt = lectures.size();
-        int pageSize = pagingService.getPageSize(lecturesCnt);
+//    private void pagingLecturesModel(Long page, Model model, List<Lecture> lectures) {
+//        int lecturesCnt = lectures.size();
+//        int pageSize = pagingService.getPageSize(lecturesCnt);
+//        int currentPage = page == null? 1: page.intValue();
+//        int startPage = 1;
+//        int endPage = pageSize;
+//
+//        List<Lecture> showLectures = pagingService.filteringLectures(lectures, currentPage);
+//        List<Integer> showPages = pagingService.getShowPages(lecturesCnt, currentPage);
+//
+//        model.addAttribute("lectures", showLectures);
+//        model.addAttribute("pages", showPages);
+//        model.addAttribute("startPage", startPage);
+//        model.addAttribute("endPage", endPage);
+//    }
+    private void pagingMyLectures(Long page, Model model, Student student) {
+        PageDto pageDto = getPageDto(page, student);
+
+        List<Lecture> pagingLectures = lectureService.findByPageMyLectures(student, pageDto.getPage(), pageDto.getPageSize());
+        List<Integer> showPages = pagingService.getShowPages(pageDto);
+
+        addModelToPagingLecturesAndPages(model, pageDto, pagingLectures, showPages);
+        model.addAttribute("student", student);
+    }
+    private void pagingMyLectures(Long page, Model model, Instructor instructor) {
+        PageDto pageDto = getPageDto(page, instructor);
+
+        List<Lecture> pagingLectures = lectureService.findByPageMyLectures(instructor, pageDto.getPage(), pageDto.getPageSize());
+        List<Integer> showPages = pagingService.getShowPages(pageDto);
+
+        addModelToPagingLecturesAndPages(model, pageDto, pagingLectures, showPages);
+        model.addAttribute("instructor", instructor);
+    }
+
+    private PageDto getPageDto(Long page, Student student) {
+        int pageSize = 10;
+        int showPageUnit = 3;
+        int lecturesCnt = student.getApplyingLectures().size();
         int currentPage = page == null? 1: page.intValue();
-        int startPage = 1;
-        int endPage = pageSize;
+        int endPage = student.getApplyingLectures().isEmpty()? 1 : getMaxPage(lecturesCnt, pageSize);
 
-        List<Lecture> showLectures = pagingService.filteringLectures(lectures, currentPage);
-        List<Integer> showPages = pagingService.getShowPages(lecturesCnt, currentPage);
+        return new PageDto(currentPage, endPage, showPageUnit, pageSize);
+    }
+    private PageDto getPageDto(Long page, Instructor instructor) {
+        int pageSize = 10;
+        int showPageUnit = 3;
+        int lecturesCnt = instructor.getLectures().size();
+        int currentPage = page == null? 1: page.intValue();
+        int endPage = instructor.getLectures().isEmpty()? 1 : getMaxPage(lecturesCnt, pageSize);
 
-        model.addAttribute("lectures", showLectures);
+        return new PageDto(currentPage, endPage, showPageUnit, pageSize);
+    }
+
+    private void addModelToPagingLecturesAndPages(Model model, PageDto pageDto, List<Lecture> pagingLectures, List<Integer> showPages) {
+        model.addAttribute("lectures", pagingLectures);
         model.addAttribute("pages", showPages);
-        model.addAttribute("startPage", startPage);
-        model.addAttribute("endPage", endPage);
+        model.addAttribute("startPage", pageDto.getStartPage());
+        model.addAttribute("endPage", pageDto.getEndPage());
+    }
+
+    private static int getMaxPage(int lecturesCnt, int pageSize) {
+        return (int) Math.ceil((double) lecturesCnt / (double) pageSize);
     }
 
     private Instructor findInstructor(HttpSession session) {
@@ -93,7 +155,6 @@ public class MyLectureController {
         Instructor instructor = memberService.findInstructor(memberId);
         return instructor;
     }
-
     private Student findStudent(HttpSession session) {
         Long memberId = (Long) session.getAttribute(SessionConst.LOGIN_ID);
         Student student = memberService.findStudent(memberId);
