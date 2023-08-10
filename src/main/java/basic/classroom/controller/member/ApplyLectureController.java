2f122f9b7b4c68256e633af34e4bba0f2de38aa7
Jiin <jiin.bae99@gmail.com>
2023-08-10 14:09:51 +0900

    :art: static method에서 static 제거

--- src/main/java/basic/classroom/controller/member/ApplyLectureController.java
+++ src/main/java/basic/classroom/controller/member/ApplyLectureController.java
@@ -28,44 +28,6 @@ public class ApplyLectureController {
     private final LectureService lectureService;
     private final PagingService pagingService;
 
-//    @GetMapping("/student/find/lectures")
-//    public String findLectures(@ModelAttribute SearchConditionDto searchConditionDto, BindingResult bindingResult, HttpSession session, Model model) {
-//        Student student = findStudent(session);
-//        List<Lecture> lectures = lectureService.findPersonalizedLectures(searchConditionDto);
-//
-//        int lecturesCnt = lectures.size();
-//        int pageSize = pagingService.getPageSize(lectures.size());
-//        int currentPage = searchConditionDto.getPage() == null ? 1 : searchConditionDto.getPage().intValue();
-//
-//        List<Lecture> showLectures = pagingService.filteringLectures(lectures, currentPage);
-//        List<Integer> showPages = pagingService.getShowPages(lecturesCnt, currentPage);
-//
-//        ShowFindLecturesDto showFindLecturesDto = new ShowFindLecturesDto(pageSize, showLectures, showPages);
-//
-//        // Validation
-//        String condition = searchConditionDto.getCondition();
-//        String text = searchConditionDto.getText();
-//        if (condition != null && !condition.isBlank()) {
-//            if (text.isBlank()) {
-//                bindingResult.reject("NoSuchFieldError", "조건 검색 시 검색명을 함께 입력해주세요.");
-//                return AddModelToShowFindLecturesDto(model, student, searchConditionDto, showFindLecturesDto);
-//            }
-//        }
-//
-//        if (text != null && !text.isBlank()) {
-//            if (condition == null || condition.isBlank()) {
-//                bindingResult.reject("NoSuchFieldError", "조건 검색 시 검색 조건을 설정해주세요.");
-//                return AddModelToShowFindLecturesDto(model, student, searchConditionDto, showFindLecturesDto);
-//            }
-//        }
-//
-//        return AddModelToShowFindLecturesDto(model, student, searchConditionDto, showFindLecturesDto);
-//    }
-
-    public int getEndPage(int lecturesCnt) {
-        return (int) Math.ceil((double) lecturesCnt / 10);
-    }
-
     @GetMapping("/student/find/lectures")
     public String findLectures(@ModelAttribute SearchConditionDto searchConditionDto, BindingResult bindingResult, HttpSession session, Model model) {
         Student student = findStudent(session);
@@ -99,6 +61,7 @@ public class ApplyLectureController {
 
         return AddModelToShowFindLecturesDto(model, student, searchConditionDto, showFindLecturesDto);
     }
+
     @PostMapping("/student/add/lecture/{lectureId}")
     public String addLecture(@PathVariable Long lectureId, HttpSession session) {
         Student student = findStudent(session);
@@ -113,18 +76,6 @@ public class ApplyLectureController {
         return "redirect:/student/lectures";
     }
 
-//    @Getter
-//    private class ShowFindLecturesDto {
-//        private int pageSize;
-//        private List<Lecture> showLectures;
-//        private List<Integer> showPages;
-//
-//        public ShowFindLecturesDto(int pageSize, List<Lecture> showLectures, List<Integer> showPages) {
-//            this.pageSize = pageSize;
-//            this.showLectures = showLectures;
-//            this.showPages = showPages;
-//        }
-//    }
     @Getter
     private class ShowFindLecturesDto {
         private int endPage;
@@ -144,15 +95,7 @@ public class ApplyLectureController {
         return student;
     }
 
-//    private static String AddModelToShowFindLecturesDto(Model model, Student student, SearchConditionDto searchConditionDto, ShowFindLecturesDto showFindLecturesDto) {
-//        addModelToStudentAndLectures(model, student, showFindLecturesDto.getShowLectures());
-//        addModelToPages(model, showFindLecturesDto.getShowPages(), showFindLecturesDto.getPageSize());
-//        addModelToLectureStatus(model);
-//        addModelToSearchCondition(searchConditionDto, model);
-//
-//        return "member/student/findLecture";
-//    }
-    private static String AddModelToShowFindLecturesDto(Model model, Student student, SearchConditionDto searchConditionDto, ShowFindLecturesDto showFindLecturesDto) {
+    private String AddModelToShowFindLecturesDto(Model model, Student student, SearchConditionDto searchConditionDto, ShowFindLecturesDto showFindLecturesDto) {
         addModelToStudentAndLectures(model, student, showFindLecturesDto.getShowLectures());
         addModelToPages(model, showFindLecturesDto.getShowPages(), showFindLecturesDto.getEndPage());
         addModelToLectureStatus(model);
@@ -161,18 +104,18 @@ public class ApplyLectureController {
         return "member/student/findLecture";
     }
 
-    private static void addModelToStudentAndLectures(Model model, Student student, List<Lecture> showLectures) {
+    private void addModelToStudentAndLectures(Model model, Student student, List<Lecture> showLectures) {
         model.addAttribute("student", student);
         model.addAttribute("lectures", showLectures);
     }
 
-    private static void addModelToSearchCondition(SearchConditionDto searchConditionDto, Model model) {
+    private void addModelToSearchCondition(SearchConditionDto searchConditionDto, Model model) {
         model.addAttribute("searchConditionDto", searchConditionDto);
         model.addAttribute("lectureStatusList", LectureStatusSearchCondition.values());
         model.addAttribute("lectureSearchConditions", LectureSearchCondition.values());
     }
 
-    private static void addModelToPages(Model model, List<Integer> showPages, int pageSize) {
+    private void addModelToPages(Model model, List<Integer> showPages, int pageSize) {
         int startPage = 1;
         int endPage = pageSize;
 
@@ -181,9 +124,13 @@ public class ApplyLectureController {
         model.addAttribute("endPage", endPage);
     }
 
-    private static void addModelToLectureStatus(Model model) {
+    private void addModelToLectureStatus(Model model) {
         model.addAttribute("lectureStatusReady", LectureStatus.READY);
         model.addAttribute("lectureStatusOpen", LectureStatus.OPEN);
         model.addAttribute("lectureStatusFull", LectureStatus.FULL);
+    }
+
+    private int getEndPage(int lecturesCnt) {
+        return (int) Math.ceil((double) lecturesCnt / 10);
     }
 }
