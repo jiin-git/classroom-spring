7f7963493515fd6aebffc7c3f8c80b313b8cd322
Jiin <jiin.bae99@gmail.com>
2024-04-11 22:34:30 +0900

    Domain - LectureStudentMapper 주석, 미사용 메서드 삭제

--- src/main/java/basic/classroom/domain/LectureStudentMapper.java
+++ src/main/java/basic/classroom/domain/LectureStudentMapper.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
-// Mapper의 역할? ManyToMany의 관계를 one to many - many to one 으로 변경해주기 위함
-// student / lecture에서 조회 시 mapper에 연결된 lecture / student를 조회할 수 있게 변경?
 @Entity
 @Builder
 @Getter
@@ -25,16 +23,10 @@ public class LectureStudentMapper {
     @JsonIgnore
     private Student student;
 
-    public Long getStudentId() {
-        return student.getId();
-    }
     public void addStudent(Student student) {
         this.student = student;
     }
 
-    public Long getLectureId() {
-        return lecture.getId();
-    }
     public void addLecture(Lecture lecture) {
         this.lecture = lecture;
     }
