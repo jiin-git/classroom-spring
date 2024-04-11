abe946a0de1e2000f48e6a596beca1378a4be21d
Jiin <jiin.bae99@gmail.com>
2024-04-11 22:37:41 +0900

    Domain - Student method 위치 수정, 일부 method 삭제 추가, 주석 삭제

--- src/main/java/basic/classroom/domain/Student.java
+++ src/main/java/basic/classroom/domain/Student.java
@@ -6,9 +6,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -28,48 +26,8 @@ public class Student {
     @Embedded
     private ProfileImage profileImage;
 
-    // key : lecture id -> mapper id 변경(mapper를 통해서만 강의 엔티티에 접근할 수 있다)
     @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
     private Map<Long, LectureStudentMapper> applyingLectures;
-
-    // applyingLectures - key: lectureId -> mapperId 변경
-    public void applyLecture(LectureStudentMapper mapper, Lecture lecture) {
-        Long mapperId = mapper.getId();
-        applyingLectures.put(mapperId, mapper);
-
-        mapper.addStudent(this);
-        lecture.addStudent(mapper);
-    }
-    public void cancelLecture(Lecture lecture) {
-        applyingLectures.remove(lecture.getId());
-        lecture.removeStudent(id);
-    } /* 삭제 예정 */
-    public void cancelLecture(Long mapperId, Lecture lecture) {
-        applyingLectures.remove(mapperId);
-        lecture.removeStudent(mapperId);
-    }
-
-    public LectureStudentMapper getMapper(Long mapperId) {
-        boolean containsKey = applyingLectures.containsKey(mapperId);
-        if (!containsKey) {
-            throw new LectureNotFoundException(ErrorCode.LECTURE_NOT_FOUND);
-        }
-        return applyingLectures.get(mapperId);
-    }
-    public Lecture getLecture(Long mapperId) {
-        log.info("mapper Id = {}", mapperId);
-        boolean containsKey = applyingLectures.containsKey(mapperId);
-        if (!containsKey) {
-            throw new LectureNotFoundException(ErrorCode.LECTURE_NOT_FOUND);
-        }
-
-        return applyingLectures.get(mapperId).getLecture();
-    }
-    public List<Lecture> findAllLectures() {
-        List<Lecture> lectures = new ArrayList<>();
-        applyingLectures.forEach((key, mapper) -> lectures.add(mapper.getLecture()));
-        return lectures;
-    }
 
     public static Student createStudent(Member member) {
         return Student.builder().member(member).applyingLectures(new HashMap<>()).build();
@@ -90,5 +48,39 @@ public class Student {
     }
     public void clearProfileImage() {
         this.profileImage = null;
+    }
+
+    public LectureStudentMapper getMapper(Long mapperId) {
+        boolean containsKey = applyingLectures.containsKey(mapperId);
+        if (!containsKey) {
+            throw new LectureNotFoundException(ErrorCode.LECTURE_NOT_FOUND);
+        }
+        return applyingLectures.get(mapperId);
+    }
+    public Map<Long, Long> getAllMyLectureIds() {
+        Map<Long, Long> lectureIds = new HashMap<>();
+        applyingLectures.forEach((key, mapper) -> lectureIds.put(mapper.getLecture().getId(), key));
+        return lectureIds;
+    }
+
+    public void applyLecture(LectureStudentMapper mapper, Lecture lecture) {
+        Long mapperId = mapper.getId();
+        applyingLectures.put(mapperId, mapper);
+
+        mapper.addStudent(this);
+        lecture.addStudent(mapper);
+    }
+    public Lecture getLecture(Long mapperId) {
+        boolean containsKey = applyingLectures.containsKey(mapperId);
+        if (!containsKey) {
+            throw new LectureNotFoundException(ErrorCode.LECTURE_NOT_FOUND);
+        }
+
+        return applyingLectures.get(mapperId).getLecture();
+    }
+    public void cancelLecture(Long mapperId) {
+        Lecture lecture = getLecture(mapperId);
+        applyingLectures.remove(mapperId);
+        lecture.removeStudent(mapperId);
     }
 }
