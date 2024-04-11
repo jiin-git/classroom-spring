37a3c9038a93f404595f11e2f9cc27f8810c5252
Jiin <jiin.bae99@gmail.com>
2024-04-11 22:20:20 +0900

    Dto - AddLectureRequest personnel type 수정 및 @NotNull 추가

--- src/main/java/basic/classroom/dto/AddLectureRequest.java
+++ src/main/java/basic/classroom/dto/AddLectureRequest.java
@@ -3,10 +3,7 @@ package basic.classroom.dto;
 import basic.classroom.domain.LectureStatus;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.validator.constraints.Range;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,8 +14,9 @@ import org.springframework.web.multipart.MultipartFile;
 public class AddLectureRequest {
     @Size(min = 1, max = 15, message = "이름을 1 ~ 15자리 사이로 입력해주세요.")
     private String name;
+    @NotNull(message = "정원을 1 ~ 100명 사이로 입력해주세요.")
     @Range(min = 1, max = 100, message = "정원을 1 ~ 100명 사이로 입력해주세요.")
-    private int personnel;
+    private Long personnel;
     @NotNull(message = "강의 상태를 설정해주세요.")
     private LectureStatus lectureStatus;
     private MultipartFile imageFile;
