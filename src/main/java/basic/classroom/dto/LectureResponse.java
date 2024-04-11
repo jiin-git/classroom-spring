321eb4323e0794887e7c20e7c04975607e66929e
Jiin <jiin.bae99@gmail.com>
2024-04-11 22:29:03 +0900

    Dto - LectureResponse class의 LectureDetailsResponse, LectureBasicResponse 를 Member Roles 에 따른 Dto로 추가 및 변경, NoArgsConstructor AccessLevel PROTECTED 변경

--- src/main/java/basic/classroom/dto/LectureResponse.java
+++ src/main/java/basic/classroom/dto/LectureResponse.java
@@ -8,15 +8,31 @@ public class LectureResponse {
 
     @Builder
     @Getter
-    @NoArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @AllArgsConstructor
-    public static class LectureDetailsResponse {
+    public static class InstructorLectureBasicResponse {
+        private LectureBasicInfo lectureBasicInfo;
+        private ProfileImage profileImage;
+
+        public static InstructorLectureBasicResponse fromLecture(Lecture lecture) {
+            return InstructorLectureBasicResponse.builder()
+                    .lectureBasicInfo(LectureBasicInfo.fromLecture(lecture))
+                    .profileImage(ProfileImage.fromLecture(lecture))
+                    .build();
+        }
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    public static class InstructorLectureDetailsResponse {
         private ProfileImage profileImage;
         private LectureBasicInfo lectureBasicInfo;
         private InstructorInfo instructorInfo;
 
-        public static LectureDetailsResponse fromLecture(Lecture lecture) {
-            return LectureDetailsResponse.builder()
+        public static InstructorLectureDetailsResponse fromLecture(Lecture lecture) {
+            return InstructorLectureDetailsResponse.builder()
                     .lectureBasicInfo(LectureBasicInfo.fromLecture(lecture))
                     .instructorInfo(InstructorInfo.fromLecture(lecture))
                     .profileImage(ProfileImage.fromLecture(lecture)).build();
@@ -25,17 +41,65 @@ public class LectureResponse {
 
     @Builder
     @Getter
-    @NoArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
     @AllArgsConstructor
-    public static class LectureBasicResponse {
+    public static class StudentLectureBasicResponse {
+        private Long mapperId;
         private LectureBasicInfo lectureBasicInfo;
+        private String instructorName;
         private ProfileImage profileImage;
 
-        public static LectureBasicResponse fromLecture(Lecture lecture) {
-            return LectureBasicResponse.builder()
+        public static StudentLectureBasicResponse fromLecture(Lecture lecture, Long mapperId) {
+            return StudentLectureBasicResponse.builder()
+                    .mapperId(mapperId)
                     .lectureBasicInfo(LectureBasicInfo.fromLecture(lecture))
+                    .instructorName(lecture.getInstructor().getMember().getName())
                     .profileImage(ProfileImage.fromLecture(lecture))
                     .build();
         }
     }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    public static class StudentLectureDetailsResponse {
+        private Long mapperId;
+        private LectureBasicInfo lectureBasicInfo;
+        private InstructorInfo instructorInfo;
+        private ProfileImage profileImage;
+
+        public static StudentLectureDetailsResponse fromLecture(Lecture lecture, Long mapperId) {
+            return StudentLectureDetailsResponse.builder()
+                    .mapperId(mapperId)
+                    .lectureBasicInfo(LectureBasicInfo.fromLecture(lecture))
+                    .instructorInfo(InstructorInfo.fromLecture(lecture))
+                    .profileImage(ProfileImage.fromLecture(lecture)).build();
+        }
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @AllArgsConstructor
+    public static class StudentFindLecturesResponse {
+        private Long mapperId;
+        private Boolean isApplied;
+        private LectureBasicInfo lectureBasicInfo;
+        private InstructorInfo instructorInfo;
+        private ProfileImage profileImage;
+
+        public static StudentFindLecturesResponse fromLecture(Lecture lecture, Long mapperId) {
+            StudentFindLecturesResponseBuilder studentFindLecturesResponseBuilder = StudentFindLecturesResponse.builder()
+                    .lectureBasicInfo(LectureBasicInfo.fromLecture(lecture))
+                    .instructorInfo(InstructorInfo.fromLecture(lecture))
+                    .profileImage(ProfileImage.fromLecture(lecture));
+
+            if (mapperId == null) {
+                return studentFindLecturesResponseBuilder.isApplied(false).build();
+            }
+            return studentFindLecturesResponseBuilder.mapperId(mapperId).isApplied(true).build();
+        }
+    }
+
 }
