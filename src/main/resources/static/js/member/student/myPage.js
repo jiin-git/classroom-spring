3811d6cc14fbd6b66f3a45e25374009d72ded1ac
Jiin <jiin.bae99@gmail.com>
2024-04-12 21:30:56 +0900

    Resources - student myPage.js 오류 수정

--- src/main/resources/static/js/member/student/myPage.js
+++ src/main/resources/static/js/member/student/myPage.js
@@ -23,10 +23,10 @@ function addProfileImage(data) {
     const profileImage = document.getElementById('profile-image');
     const imageFile = data.profileImage;
     if (imageFile) {
-        currentImage.innerHTML +=
+        profileImage.innerHTML +=
             `<img src="data:${imageFile.dataType};base64,${imageFile.imageData}" class="img-fluid">`;
     } else {
-        currentImage.innerHTML +=
+        profileImage.innerHTML +=
             `<div class="profile-img-container">
                 <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-person-fill default-profile-img" viewBox="0 0 16 16">
                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
@@ -39,7 +39,7 @@ function addMyPageInfo(data) {
     const loginId = document.getElementById('mypage-info-id');
     const email = document.getElementById('mypage-info-email');
 
-    name.value = `${data.name}`;
-    loginId.value = `${data.loginId}`;
-    email.value = `${data.email}`;
+    name.innerHTML = `${data.name}`;
+    loginId.innerHTML = `${data.loginId}`;
+    email.innerHTML = `${data.email}`;
 }
