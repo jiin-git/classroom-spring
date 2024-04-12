28a5bfddeebf4558d7b63b5cf0cf261e409fc953
Jiin <jiin.bae99@gmail.com>
2024-04-12 22:01:27 +0900

    Mvc Controller - Home Controller authorities 수정, jwtTokenProvider 삭제

--- src/main/java/basic/classroom/controller/mvc/HomeController.java
+++ src/main/java/basic/classroom/controller/mvc/HomeController.java
@@ -1,9 +1,7 @@
 package basic.classroom.controller.mvc;
 
-import basic.classroom.config.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,18 +14,13 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    private final JwtTokenProvider jwtTokenProvider;
-
     @GetMapping("/")
     public String home(@AuthenticationPrincipal UserDetails userDetails) {
         if (userDetails == null) {
             return "home";
         }
 
-        String loginId = userDetails.getUsername();
-        Authentication authentication = jwtTokenProvider.getAuthentication(loginId);
-        List<? extends GrantedAuthority> authorities = authentication.getAuthorities().stream().toList();
-
+        List<? extends GrantedAuthority> authorities = userDetails.getAuthorities().stream().toList();
         if (authorities.stream().anyMatch(auth -> auth.getAuthority().equals("STUDENT"))) {
             return "redirect:/student/lectures";
         } else if (authorities.stream().anyMatch(auth -> auth.getAuthority().equals("INSTRUCTOR"))) {
