e0002ac240b86b22a9d87d71d9a9ae60a2650186
Jiin <jiin.bae99@gmail.com>
2024-04-12 16:41:15 +0900

    Mvc Controller - Members Student Controller Commit

--- src/main/java/basic/classroom/controller/mvc/members/v2/student/StudentApplyLectureController.java
+++ src/main/java/basic/classroom/controller/mvc/members/v2/student/StudentApplyLectureController.java
@@ -7,7 +7,6 @@ import basic.classroom.dto.SearchLectureRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,50 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/student/find/lectures")
 public class StudentApplyLectureController {
     @GetMapping("")
-    public String findAndApplyLectures(@ModelAttribute SearchLectureRequest searchLectureRequest, BindingResult bindingResult, Model model) {
-        validateSearchCondition(searchLectureRequest, bindingResult);
+    public String findAndApplyLectures(@ModelAttribute SearchLectureRequest searchLectureRequest, Model model) {
         addLectureStatusToModel(model);
-        addSearchConditionToModel(model, searchLectureRequest);
+        addSearchConditionToModel(model);
         return "member/student/v2/findLecture";
     }
-
-    private void validateSearchCondition(SearchLectureRequest searchLectureRequest, BindingResult bindingResult) {
-        String condition = searchLectureRequest.getCondition();
-        String text = searchLectureRequest.getText();
-        if (condition != null && !condition.isBlank()) {
-            if (text.isBlank()) {
-//                ErrorCode errorCode = ErrorCode.FAILED_FIND_LECTURE_BY_SEARCH_CONDITION;
-//                String errorCodeName = ErrorCode.FAILED_FIND_LECTURE_BY_SEARCH_CONDITION.name();
-//                String errorMessage = ErrorCode.FAILED_FIND_LECTURE_BY_SEARCH_CONDITION.getMessage();
-//                bindingResult.rejectValue("text", errorCodeName, new Object[]{errorCode}, errorMessage);
-//                bindingResult.rejectValue("text", "NoSuchFieldError", "조건 검색 시 검색명을 함께 입력해주세요.");
-                bindingResult.reject("NoSuchFieldError", "조건 검색 시 검색명을 함께 입력해주세요.");
-            }
-        }
-
-        if (text != null && !text.isBlank()) {
-            if (condition == null || condition.isBlank()) {
-//                ErrorCode errorCode = ErrorCode.FAILED_FIND_LECTURE_BY_SEARCH_CONDITION;
-//                String errorCodeName = ErrorCode.FAILED_FIND_LECTURE_BY_SEARCH_CONDITION.name();
-//                String errorMessage = ErrorCode.FAILED_FIND_LECTURE_BY_SEARCH_CONDITION.getMessage();
-//                bindingResult.rejectValue("text", errorCodeName, new Object[]{errorCode}, errorMessage);
-//                bindingResult.rejectValue("condition","NoSuchFieldError", "조건 검색 시 검색 조건을 설정해주세요.");
-                bindingResult.reject("NoSuchFieldError", "조건 검색 시 검색 조건을 설정해주세요.");
-            }
-        }
-    }
-
-    private void addSearchConditionToModel(Model model, SearchLectureRequest searchLectureRequest) {
-        String status = searchLectureRequest.getStatus();
-        String condition = searchLectureRequest.getCondition();
-
-        if (status != null && !status.isBlank() || condition != null && !condition.isBlank()) {
-            model.addAttribute("searchLectureRequest", searchLectureRequest);
-        }
-        else {
-            model.addAttribute("searchLectureRequest", new SearchLectureRequest());
-        }
-
+    private void addSearchConditionToModel(Model model) {
         model.addAttribute("lectureStatusList", LectureStatusSearchCondition.values());
         model.addAttribute("lectureSearchConditions", LectureSearchCondition.values());
     }
--- src/main/java/basic/classroom/controller/mvc/members/v2/student/StudentMyPageController.java
+++ src/main/java/basic/classroom/controller/mvc/members/v2/student/StudentMyPageController.java
@@ -1,6 +1,5 @@
 package basic.classroom.controller.mvc.members.v2.student;
 
-import basic.classroom.dto.UpdatePasswordRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,7 +22,6 @@ public class StudentMyPageController {
 
     @GetMapping("/update/my-page/password")
     public String updatePasswordForm(Model model) {
-        model.addAttribute("updatePasswordForm", new UpdatePasswordRequest());
         return "member/student/v2/updatePassword";
     }
 }
